de4d79bd4d43521a5df578215d16aef60d422a45
Lindsay Ward <lindsay.ward@jcu.edu.au>
2024-02-26 09:23:58 +1000

    Create movies2see starter

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+"""
+Name:
+Date started:
+GitHub URL (of this assignment):
+Remember to NEVER make this assignment repo public
+"""
+
+
+def main():
+    """..."""
+
+
+if __name__ == '__main__':
+    main()
